da2018b4e13b1a2693e510d013d05ac5b99db9f9
joneshf <jones3.hardy@gmail.com>
2014-06-24 00:07:24 -0700

    Added axis support and fleshed out types a bit more.

--- README.md
+++ README.md
@@ -4,26 +4,42 @@
 
 ### Types
 
+    data Axis dim
+
     data C3 :: *
+
+    type C3Data  = { c3Type :: C3Type, name :: String, values :: [Number] }
 
     data C3Type where
       Bar :: C3Type
       Line :: C3Type
       Pie :: C3Type
 
-    type Options  = { c3Data :: [{ c3Type :: C3Type, name :: String, values :: [Number] }], bindto :: String }
+    type Options  = { yAxis :: Axis YDim, xAxis :: Axis XDim, c3Data :: [C3Data], bindto :: String }
+
+    data XDim
+
+    data YDim
 
 
 ### Type Class Instances
+
+    instance eqC3Type :: Eq C3Type
 
     instance showC3Type :: Show C3Type
 
 
 ### Values
 
+    c3Data :: C3Data
+
     generate :: forall eff. Options -> Eff (dom :: DOM | eff) C3
 
     options :: Options
+
+    xAxis :: String -> [String] -> Axis XDim
+
+    yAxis :: String -> [String] -> Axis YDim
 
 
 
--- src/Graphics/C3.purs
+++ src/Graphics/C3.purs
@@ -1,15 +1,26 @@
 module Graphics.C3
   ( C3()
   , Options()
+  , C3Data(..)
   , C3Type(..)
+  , Axis()
+  , XDim()
+  , YDim()
   , generate
+  , c3Data
   , options
+  , xAxis
+  , yAxis
   ) where
 
   import Control.Monad.Eff
   import DOM
 
   foreign import data C3 :: *
+
+  data Axis dim = Axis String String [String]
+  data XDim = XDim
+  data YDim = YDim
 
   data C3Type = Bar
               | Line
@@ -20,20 +31,51 @@ module Graphics.C3
     show Line = "line"
     show Pie  = "pie"
 
+  instance eqC3Type :: Eq C3Type where
+    (==) Bar  Bar  = true
+    (==) Line Line = true
+    (==) Pie  Pie  = true
+    (==) _    _    = false
+
+    (/=) t    t'   = not (t == t')
+
+  type C3Data =
+    { values :: [Number]
+    , name   :: String
+    , c3Type :: C3Type
+    }
   type Options =
     { bindto :: String
-    , c3Data :: [{ values :: [Number]
-                 , name   :: String
-                 , c3Type :: C3Type
-                 }
-                ]
+    , c3Data :: [C3Data]
+    , xAxis :: Axis XDim
+    , yAxis :: Axis YDim
     }
 
   options :: Options
   options =
     { bindto: ""
     , c3Data: []
+    , xAxis: xAxis "" []
+    , yAxis: yAxis "" []
     }
+  c3Data :: C3Data
+  c3Data =
+    { c3Type: Bar
+    , name: ""
+    , values: []
+    }
+
+  xAxis :: String -> [String] -> Axis XDim
+  xAxis = Axis "x"
+
+  yAxis :: String -> [String] -> Axis YDim
+  yAxis = Axis "y"
+
+  axisName :: forall dim. Axis dim -> String
+  axisName (Axis _ name _) = name
+
+  axisData :: forall dim. Axis dim -> [String]
+  axisData (Axis _ _ ds) = ds
 
   foreign import generate
     "function generate(opts) {\
@@ -58,8 +100,33 @@ module Graphics.C3
     \    }),\
     \    types: c3Types(opts)\
     \  };\
+    \  if (axisName(opts.xAxis) !== '') {\
+    \    obj = insertAxis(obj)(opts.xAxis);\
+    \  }\
+    \  if (axisName(opts.yAxis) !== '') {\
+    \    obj = insertAxis(obj)(opts.yAxis);\
+    \  }\
     \  return obj;\
     \}" :: Options -> ActualOptions
+
+  foreign import insertAxis
+    "function insertAxis(opts) {\
+    \  return function(axis) {\
+    \    var xData = axisData(axis);\
+    \    opts.data.x = axisName(axis);\
+    \    opts.data.columns.unshift([opts.data.x].concat(xData));\
+    \    opts.axis = opts.axis || {};\
+    \    opts.axis.x = {\
+    \      type: 'category',\
+    \      tick: {\
+    \        format: function(i) {\
+    \          return xData[i];\
+    \        }\
+    \      }\
+    \    };\
+    \    return opts;\
+    \  }\
+    \}" :: forall dim. ActualOptions -> Axis dim -> ActualOptions
 
   foreign import c3Types
     "function c3Types(opts) {\
